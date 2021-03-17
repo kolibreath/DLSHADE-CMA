ca6cdbdbf9a250b9c9bcc72755f4adc6204b03c5
kolibreath <734780178@qq.com>
2021-03-17 10:25:41 +0800

    [MOP] substract f and cr generation from lshade.m

--- lshade.m
+++ lshade.m
@@ -90,24 +90,7 @@ for func = 1:30
             mu_f = memory_sf(mem_rand_index);
             mu_cr = memory_scr(mem_rand_index);
 
-            %% for generating crossover rate
-            cr = normrnd(mu_cr, 0.1);
-            term_pos = find(mu_cr == -1); % whenever cr is set to terminal value
-            cr(term_pos) = 0;
-            cr = min(cr, 1);
-            cr = max(cr, 0);
-
-            %% for generating scaling factor
-            % TODO  should be randc in original paper
-            f = mu_f + 0.1 * tan(pi * (rand(popsize, 1) - 0.5));
-            pos = find(f <= 0);
-
-            while ~isempty(pos)
-                f(pos) = mu_f(pos) + 0.1 * tan(pi * (rand(length(pos), 1) - 0.5));
-                pos = find(f <= 0);
-            end
-
-            f = min(f, 1);
+            [f, cr] = gnFCR(popsize);
 
             r0 = [1:popsize];
             popAll = [pop; archive.pop];
