aa7a89c3c6618b61849f0c03d17838157d8a2299
kolibreath <734780178@qq.com>
2021-03-17 09:47:52 +0800

    [MOD]substract f cr generation

--- gnFCR.m
+++ gnFCR.m
@@ -8,10 +8,22 @@ function [f, cr] = gnFCR(popsize)
 
     
      %% for generating crossover rate
-      cr = normrnd(mu_cr, 0.1);
-      term_pos = find(mu_cr == -1);       % whenever cr is set to terminal value
-      cr(term_pos) = 0;
-      cr = min(cr, 1);
-      cr = max(cr, 0);
+     cr = normrnd(mu_cr, 0.1);
+     term_pos = find(mu_cr == -1);       % whenever cr is set to terminal value
+     cr(term_pos) = 0;
+     cr = min(cr, 1);
+     cr = max(cr, 0);
+      
+     %% for generating scaling factor
+     % TODO  should be randc in original paper
+     f = mu_f + 0.1 * tan(pi * (rand(popsize, 1) - 0.5));
+     pos = find(f <= 0);
+
+     while ~isempty(pos)
+         f(pos) = mu_f(pos) + 0.1 * tan(pi * (rand(length(pos), 1) - 0.5));
+         pos = find(f <= 0);
+    end
+
+    f = min(f, 1);
 end
 
